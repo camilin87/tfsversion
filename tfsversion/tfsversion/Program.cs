ba2132793859f2a9f61b6fd1023716e621da4cec
csanchez <csanchez@carnival.com>
2014-06-19 15:13:43 -0400

    read the tfs version

---
+++ tfsversion/tfsversion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.TeamFoundation.Build.Client;
+using Microsoft.TeamFoundation.Client;
+
+namespace tfsversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Pass your tfs url as an argument. e.g. http://YOUR_TFS_SERVER:8080/tfs/YOUR_COLLECTION/");
+            }
+            else
+            {
+                var tfs = new TfsTeamProjectCollection(new Uri(args[0]));
+                var tfsb = tfs.GetService<IBuildServer>();
+                var buildServerVersion = tfsb.BuildServerVersion;
+                Console.WriteLine(buildServerVersion);
+            }
+        }
+    }
+}
